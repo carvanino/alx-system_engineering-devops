7428a220e5df2799509b8ab267f8014b51b3769c
Akinola <akinolatofunmi04@gmail.com>
2022-12-19 23:19:25 +0000

    Task 2 - Adda custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Automate the task to create a custom HTT header response with Puppet
+
+exec { 'nginx install':
+path    => '/usr/bin',
+command => 'sudo apt update && sudo apt-get -y install nginx',
+}
+
+exec { 'Make directories':
+path    => '/usr/bin',
+command => 'sudo mkdir -p /var/www/html && sudo chown -R "$USER":"$USER" /var/www/html',
+}
+
+# exec { 'Make file':
+# path    => '/usr/bin',
+# command => 'echo "Hello World!" > /var/www/html/index.html'
+# }
+
+file { '/var/www/html/index.html':
+mode    => '0755',
+content => 'Hello World!',
+}
+
+exec { 'block server':
+path    => '/usr/bin',
+command => 'echo "
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+
+	index index.html index.htm index.nginx-debian.html;
+
+	add_header X-Served-By $HOSTNAME;
+
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+}" | sudo tee /etc/nginx/sites-available/default'
+}
+
+exec { 'Restart nginx':
+path    => '/usr/bin',
+command => 'sudo service nginx restart',
+}
