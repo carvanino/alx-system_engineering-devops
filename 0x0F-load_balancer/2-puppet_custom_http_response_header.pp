b73ab81c2377092f938150d7dae8536ede5bae9c
Akinola <akinolatofunmi04@gmail.com>
2022-12-19 23:46:52 +0000

    Fix hostname variable

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -32,7 +32,7 @@ server {
 
 	index index.html index.htm index.nginx-debian.html;
 
-	add_header X-Served-By $HOSTNAME;
+	add_header X-Served-By \$hostname;
 
 	location /redirect_me {
 		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
