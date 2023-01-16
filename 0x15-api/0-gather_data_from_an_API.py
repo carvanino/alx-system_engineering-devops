b156e9737304041614e99ec7dde47b9d8f4431f7
Akinola <akinolatofunmi04@gmail.com>
2023-01-16 07:31:33 -0800

    Fix requirementa

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
-Module 0-gather_data_from_an_API
 Uses a REST API return information about employees TODO List progress
 """
 
 import requests
 import sys
 
-id = int(sys.argv[1])
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-user = requests.get(user_url).json()
+if __name__ == '__main__':
+    id = int(sys.argv[1])
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    user = requests.get(user_url).json()
 
-todo_url = 'https://jsonplaceholder.typicode.com/todos'
-params = {'userId': id}
-todos = requests.get(todo_url, params=params).json()
-task = 0
-NoCompleted_tasks = 0
-completed_tasks = []
-for todo in todos:
-    task += 1
-    # print(type(todos['completed']))
-    if todo['completed'] is True:
-        NoCompleted_tasks += 1
-        completed_tasks.append(todo['title'])
-print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user['name'], NoCompleted_tasks, task))
-for tasks in completed_tasks:
-    print("\t {}".format(tasks))
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    params = {'userId': id}
+    todos = requests.get(todo_url, params=params).json()
+    task = 0
+    NoCompleted_tasks = 0
+    completed_tasks = []
+    for todo in todos:
+        task += 1
+        # print(type(todos['completed']))
+        if todo.get('completed') is True:
+            NoCompleted_tasks += 1
+            completed_tasks.append(todo.get('title'))
+    print(
+            "Employee {} is done with tasks({}/{}):".format(
+                user.get('name'), NoCompleted_tasks, task))
+    for tasks in completed_tasks:
+        print("\t {}".format(tasks))
