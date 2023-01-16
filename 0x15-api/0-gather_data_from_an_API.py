1a94be5931e5383cda7bd04c4a96f9727777f966
Akinola <akinolatofunmi04@gmail.com>
2023-01-16 14:30:08 +0000

    Revamp

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,28 +7,23 @@ import requests
 import sys
 
 id = int(sys.argv[1])
-user_url = 'https://jsonplaceholder.typicode.com/users'
-u = requests.get(user_url)
-u_dict = u.json()
-for user in u_dict:
-    if user['id'] == id:
-        user_name = user['name']
+user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+user = requests.get(user_url).json()
 
 todo_url = 'https://jsonplaceholder.typicode.com/todos'
-t = requests.get(todo_url)
-t_dict = t.json()
+params = {'userId': id}
+todos = requests.get(todo_url, params=params).json()
 task = 0
 NoCompleted_tasks = 0
 completed_tasks = []
-for todos in t_dict:
-    if todos['userId'] == id:
-        task += 1
-        # print(type(todos['completed']))
-        if todos['completed'] is True:
-            NoCompleted_tasks += 1
-            completed_tasks.append(todos['title'])
+for todo in todos:
+    task += 1
+    # print(type(todos['completed']))
+    if todo['completed'] is True:
+        NoCompleted_tasks += 1
+        completed_tasks.append(todo['title'])
 print(
         "Employee {} is done with tasks({}/{}):".format(
-            user_name, NoCompleted_tasks, task))
+            user['name'], NoCompleted_tasks, task))
 for tasks in completed_tasks:
     print("\t {}".format(tasks))
