0b7f28f5aeb05ca8d3a885d46bc3a4495ab435d0
Akinola <akinolatofunmi04@gmail.com>
2023-02-10 01:33:10 +0000

    Fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,12 @@
 # Increases Nginx file descriptor limit
 
+
+exec { 'Set worker processes to auto':
+  command => 'sed -i 2s/4/auto/ /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+
 exec {'Increase limit':
 path    => '/usr/local/bin/:/bin/',
 command => 'sed -i s/15/4096/g /etc/default/nginx',
